7f6c037e6378cb34c8808854eb0e251b1d542928
jinoSimg <jinosimg@gmail.com>
2024-01-19 17:23:39 +0900

    6th - Working on MonsterAI

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ using UnityEngine.UIElements;
 
 
 [RequireComponent(typeof(CharacterController))] // 플레이어 스크립트를 컴포넌트로 적용할시 자동으로 CharacterController컴포넌트도 적용됨
-public class Player : MonoBehaviour
+public class Player : UnitBase
 {
     enum State
     {
@@ -28,6 +28,11 @@ public class Player : MonoBehaviour
     //이동 벡터
     [SerializeField]
     private Vector3 moveForce;
+
+    [SerializeField]
+    float myMaxHp = 100;
+
+
 
     //인풋값 저장
     float axisX;
@@ -53,6 +58,7 @@ public class Player : MonoBehaviour
     //플레이어 상태를 나눌 Enum
     State playerState;
 
+    public float playerSize;
 
     // 플레이어 오프셋
     Vector3 walkWeaponPos = new Vector3(0, (float)-0.01, (float)0.01);
@@ -74,9 +80,17 @@ public class Player : MonoBehaviour
 
     void Start()
     {
+
+        Init();
+    }
+
+    protected override void Init()
+    {
         //마우스 포인터 설정
-      UnityEngine.Cursor.visible = false; // 포인터가 안보이게
-      UnityEngine.Cursor.lockState = CursorLockMode.Locked;// 포인터가 화면밖으로 못나가게
+
+        base.maxHp = myMaxHp;
+        UnityEngine.Cursor.visible = false; // 포인터가 안보이게
+        UnityEngine.Cursor.lockState = CursorLockMode.Locked;// 포인터가 화면밖으로 못나가게
 
         // 사용할 컴포넌트들
         rotateMouse = Camera.main.transform.GetComponent<RotateMouse>();
@@ -84,24 +98,36 @@ public class Player : MonoBehaviour
         animator = GetComponent<Animator>();
         weapon = weaponPoint.GetComponent<Weapon>();
 
-        if(animator)
+        if (animator)
         {
             //애니메이터를 통해서 리깅되어있는 본을 사용(머리, 가슴);
             playerChestTr = animator.GetBoneTransform(HumanBodyBones.UpperChest);
             cameraTr = animator.GetBoneTransform(HumanBodyBones.Head);
             weaponTr = animator.GetBoneTransform(HumanBodyBones.RightHand);
         }
+
+        playerSize = transform.lossyScale.y;
+        base.Init();
     }
 
+    float gravity = 10;
     public void MoveTo(Vector3 direction)
     {
         // 마우스의 Y축회전에 따라 플레이어도 회전
+
+        if (!characterController.isGrounded)
+        {
+            direction.y -= gravity * Time.deltaTime;
+        }
+
         transform.rotation = Quaternion.Euler(0, rotateMouse.eulerAnlgleY, 0);
 
         animator.SetFloat("Speed", moveSpeed);// animator의 Speed변수에 따라 블랜딩의 정도가 변함
 
         moveForce = transform.rotation * direction * moveSpeed; // 플레이어가 바라보는 방향으로 이동방향이 정해짐
         characterController.Move(moveForce * Time.deltaTime);
+
+        
     }
 
 
@@ -131,11 +157,19 @@ public class Player : MonoBehaviour
 
         if (Input.GetMouseButton(0))// 마우스 클릭시 격발
         {
-            playerState = State.Fire;
+            if(weapon.currentAmmo <=0)
+            {
+                playerState = State.Reload;
+            }
+            else
+            {
+                playerState = State.Fire;
+            }
+          
            // Debug.Log("Shoot");
         }
 
-        if(Input.GetKeyDown(KeyCode.R) || weapon.currentAmmo <=0)// 총알이 다 떨어지거나 R키 입력시 재장전
+        if(Input.GetKeyDown(KeyCode.R))// 총알이 다 떨어지거나 R키 입력시 재장전
         {
             //총알을 장전하는데 2초가 걸리는데 그사이에 업데이트문이 계속호출되서 장전을 엄청 많이하게됨
             //그렇다고 모션을그대로 넣고 장전을 하게 해주자니 장전애니메이션이 의미가 없음 2초를 기다리는게 아니라 애니메이션 이벤트로 장전을 해야하나 생각중
@@ -147,6 +181,7 @@ public class Player : MonoBehaviour
   
   
     void Update()
+
     {
 
         UpdateMove();
@@ -173,17 +208,18 @@ public class Player : MonoBehaviour
 
                 case State.Fire:
 
+               
                     weapon.Fire();
                     SetOffset(fireWeaponRot, fireWeaponPos, fireCameraPos, 0.01f,true);
-                    StopCoroutine("ReloadBullet");// 재장전중 사격하면 장전이 취소되게 설정
+                  
                     break;
 
                 case State.Reload:
                 animator.SetBool("Fire", false);// 사격 애니메이션 bool
                 animator.SetTrigger("Reload");
-                StartCoroutine(ReloadBullet(weapon.gun.reloadTime));
+                
                
-                   
+    
                     break;
              }
 
@@ -197,13 +233,36 @@ public class Player : MonoBehaviour
         Camera.main.nearClipPlane = cameraNear; // 카메라 Near값 조정
         animator.SetBool("Fire", isFire);// 사격 애니메이션 bool
     }
-    IEnumerator ReloadBullet(float reloadTime)
+
+    
+    void ReloadBullet()//애니메이션 이벤트로 호출중
     {
-        yield return new WaitForSeconds(reloadTime); // 매개변수로 받은 시간이 지나야 총알이 장전됨
+        
         weapon.currentAmmo = weapon.gun.maxAmmo;
         animator.SetTrigger("Idle");
-        Debug.Log("Reload");
+       // Debug.Log("Reload");
 
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Gear")
+        {
+            transform.localScale *= 1.1f;
+            playerSize = transform.lossyScale.y;
+        }
+
+        if(other.tag == "Enemy")
+        {
+            targetSize = other.transform.lossyScale.y;
+            base.mySize = transform.lossyScale.y;
+            Debug.Log(IsSmaller());
+        }
+
+        if(other.tag == "EnemyBullet")
+        {
+            base.GetDamage(2);
+        }
     }
 
     void Move_BoneRotation()
